bc906035a9d99b2cf720c1e672394edf993fb77e
Jean Gonçalves <jeanfpgoncalves@live.com>
2017-06-19 23:22:07 -0300

    Class Component

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,13 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import Family from './family'
-import Member from './member'
+import ClassComponent from './class.component'
 
 ReactDOM.render(
-    <Family lastName="Silva">
-        <Member name="Guilherme"/>
-        <Member name="Marcelo"/>
-        <Member name="Julian"/>
-    </Family>
+    <ClassComponent value="Componente Classe" />
 , document.getElementById('app'))
